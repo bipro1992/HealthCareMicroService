e3965794872b740154d97d040a6f9236beb69887
bipro1992 <biprokvs@gmail.com>
2019-02-24 00:51:20 +0530

    Employee Service

---
+++ Employee-service/src/main/java/hcportal/microservice/EmployeeRest.java
@@ -0,0 +1,36 @@
+package hcportal.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
+
+
+
+/**
+ * @author biprajeet
+ *
+ *This is main class of Employee Rest Spring boot app
+ *
+ */
+
+
+@EnableJpaRepositories(basePackages="hcportal.microservice.repository")
+@EnableCircuitBreaker
+@EnableCaching
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EmployeeRest {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(EmployeeRest.class, args);
+
+	}
+
+}
